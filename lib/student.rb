02f5b848d229ff0ea4609a2be23328c8d014461b
wesleyfriedman <friedman.wesley@gmail.com>
2016-10-08 16:08:26 -0400

    fix class methods

--- lib/student.rb
+++ lib/student.rb
@@ -28,19 +28,6 @@ class Student
 		DB[:conn].execute(sql)
 	end
 
-	def save
-		if self.id
-			self.update
-		else
-			sql = <<-SQL
-				INSERT INTO students(name, grade)
-				VALUES (?, ?)
-			SQL
-			DB[:conn].execute(sql, self.name, self.grade)
-			@id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-		end
-	end
-
 	def self.create(name, grade)
 		student = Student.new(name, grade)
 		student.save
@@ -62,6 +49,19 @@ class Student
 		SQL
 		result = DB[:conn].execute(sql, name)[0]
 		Student.new(result[0], result[1], result[2])
+	end
+
+	def save
+		if self.id
+			self.update
+		else
+			sql = <<-SQL
+				INSERT INTO students(name, grade)
+				VALUES (?, ?)
+			SQL
+			DB[:conn].execute(sql, self.name, self.grade)
+			@id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+		end
 	end
 
 	def update
